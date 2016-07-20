765c3db593e058a6019cd9449e40311178758b6f
drawweb <romain.nittel@groupallsun.fr>
2016-07-20 08:41:14 +0200

    efermes

---
+++ application/modules/admin/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+class Admin_IndexController extends Zend_Controller_Action
+{
+	
+	public function init()
+	{
+		$this->_helper->_layout->setLayout('auth');
+	}
+	
+    public function indexAction ()
+    {
+       
+   	}
+
+}
